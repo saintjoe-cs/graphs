bd6f922dfa7b0929a5ebf6c91ad8734f0fe5a693
Brian Capouch <brianc@saintjoe.edu>
2015-03-04 01:22:33 -0500

    Initial version of graph data structure

---
+++ Graph.js
@@ -0,0 +1,28 @@
+function Graph(v) {
+  this.vertices = v;
+  this.edges = 0;
+  this.adj = [];
+  for (var i = 0; i < this.vertices; ++i) {
+    this.adj[i] = [];
+    }
+  this.addEdge = addEdge;
+  this.showGraph = showGraph;
+}
+
+function addEdge(v,w) {
+  this.adj[v].push(w);
+  this.adj[w].push(v);
+  this.edges++;
+}
+
+function showGraph() {
+  for (var i = 0; i < this.vertices; ++i) {
+    write(i + " -> ");
+    for (var j = 0; j < this.vertices; ++j) {
+      if (this.adj[i][j] != undefined)
+        write(this.adj[i][j] + ' ');
+    }
+    print();
+  }
+}
+
